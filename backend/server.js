74bee1a87d569dcdb424c5a28b54da427c9aaf91
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-21 14:43:47 +0530

    added the models, controllers, routes and db connections

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
-const PORT = process.env.PORT || 5000
-app.get("/", (req, res) => {
-    res.send("<h1>App is healthy</h1>")
-})
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-})
+
+dotenv.config()
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("<h1>App is healthy</h1>");
+});
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
