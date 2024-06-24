de0ac1fae50d4551a892bbd094f01cc01e7945a1
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-25 01:55:36 +0530

    added the get user endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
-
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT;
 
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //     res.send("<h1>App is healthy</h1>");
 //   });
 
 app.listen(PORT, () => {
-    connectToMongoDB();
+  connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
