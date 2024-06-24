940450d1c4474d53ea44f65d674501064f6ab3a3
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-25 01:40:16 +0530

    added the send message and get message end points...

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
@@ -10,7 +12,9 @@ const PORT = process.env.PORT;
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 
 // app.get("/", (req, res) => {
