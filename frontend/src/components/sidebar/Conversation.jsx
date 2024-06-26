cab65a4b11b308f1f37e93415dc5c7ad3fc40192
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-26 17:09:43 +0530

    added the get conversations in the side bar from the backend

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,22 +1,34 @@
-const Conversation = () => {
+/* eslint-disable react/prop-types */
+
+import useConversation from "../../zustand/useConversations";
+
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-purple-500 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-purple-500 rounded p-2 py-1 cursor-pointer ${
+          isSelected ? "bg-purple-500" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src="https://i.pravatar.cc/300" alt="user avatar" />
+            <img src={conversation.profilePic} alt="user avatar" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">Test 1</p>
-            <span className="text-xl">🙏</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
 
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
