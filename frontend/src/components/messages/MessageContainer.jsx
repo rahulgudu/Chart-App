1293173e0eb220ddd285e143f6aa752d5b2d225c
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-27 14:18:37 +0530

    updated the message container according to the useConversation hooks

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,18 +1,27 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
+import useConversation from "../../zustand/useConversations";
+import { useEffect } from "react";
 const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* Header */}
           <div className="bg-purple-500 px-4 py-2 mb-2">
             <span className="label-text">
-              To: <span className="text-gray-900 font-bold">Test 1</span>
+              To:{" "}
+              <span className="text-gray-900 font-bold">
+                {selectedConversation.fullName}
+              </span>
             </span>
           </div>
 
