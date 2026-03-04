95f792d48a69dd55af5283119dabf3c3982d418c
Rahul <117711778+rahulgudu@users.noreply.github.com>
2026-03-04 09:49:12 +0000

    sentry setup

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
+import * as Sentry from "@sentry/react-native"
 const ChatsScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
+      <Button title='Try!' onPress={() => { Sentry.captureException(new Error('First error')) }} />
       <Text>ChatsScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,28 @@ import { Stack } from "expo-router";
 import "../../global.css";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://a054d757010758ca80465304c2ebb1f6@o4510974122786816.ingest.us.sentry.io/4510985539485696',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
