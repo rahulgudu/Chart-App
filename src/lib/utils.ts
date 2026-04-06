5c597d15f1f2215ace6f52949043c80f2488791e
Rahul <117711778+rahulgudu@users.noreply.github.com>
2026-04-06 12:20:47 +0000

    empty

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function getGreetingForHour(date: Date = new Date()): string {
-    const hour = date.getHours();
-
-    if (hour < 12) return "Good Morining";
-    if (hour < 17) return "Good Afternoon";
-
-    return "Good Evening";
-}
